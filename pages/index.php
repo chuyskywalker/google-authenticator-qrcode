c62bc8cd40620766ecacfdfd95719bf48650a6fd
chuyskywalker <github.com@jrm.cc>
2014-02-08 10:36:00 -0800

    Readme file with overview and index page nav

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cross-Browser QRCode generator for Javascript</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" ></script>
+<script type="text/javascript" src="jquery.qrcode-0.7.0.js"></script>
+</head>
+<body>
+
+<p><a href="generate.php">Generate a secret, define a issuer/account, and scan a QR code</a></p>
+<p><a href="validate.php">Input a known secret and the current OTP and test validation</a></p>
+
+</body>
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Google Authenticator Test
+
+The rough of this is:
+
+1. Visit `generate.php` and either a) supply a known code or b) use the generated code
+2. Change the issuer/account details as pleases you
+3. Submit the form
+5. Scan the QR code into Google Authenticator
+6. Save the Secret
+7. Go to `validate.php`
+8. Enter the Secret
+9. Enter the current (or most recent) OTP code
+10. Submit validation form and cross fingers for "CODE OK"
+
+In a real world application you would save the secret code into your database,
+preferably encrypted somehow, and extract it whenever you needed to validate
+a user at login.
